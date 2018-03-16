878db1762788b56ecaa98a3cfae36267b8713660
Sean_George <seangeorge0@gmail.com>
2018-03-15 19:27:18 -0600

    use static

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(require('helmet')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('static'));
 
 let movies = [
   {
